a77ab6da193dea2250aced82ca8afca93e8491cf
CHNAIDA AYOUB <131295282+ayoubchnaida@users.noreply.github.com>
2023-09-15 23:12:20 +0100

    Update app_gold.py

--- app_gold.py
+++ app_gold.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 import pandas as pd
-from sklearn import sklearn.ensemble
+from sklearn.ensemble import RandomForestRegressor
 # Load the Model
 model = pickle.load(open('Gold.sav', 'rb'))
 
